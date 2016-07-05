08d64da13c918c3a3c99c349da1900161664a8cc
SobchenkoI <sobchenko.ihor@gmail.com>
2016-07-05 08:50:03 +0300

    some changes

--- config/puma.rb
+++ config/puma.rb
@@ -5,6 +5,5 @@ pidfile "/var/www/budmag/shared/tmp/pids/puma.pid"
 stdout_redirect "/var/www/budmag/shared/tmp/log/stdout", "/var/www/budmag/shared/tmp/log/stderr"
 threads 2, 16
 workers 2
-bind "unix:/var/www/budmag/shared/tmp/sockets/puma.sock"
+bind "unix:///var/www/budmag/shared/tmp/sockets/puma.sock"
 daemonize true
-
